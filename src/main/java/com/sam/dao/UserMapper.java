20c5ba08ae7a807c2edc9f420eff01b8ab58b245
limq <limq@cgnfs.com.cn>
2020-04-20 15:26:21 +0800

    入门小例子 test

---
+++ src/main/java/com/sam/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.sam.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sam.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
--- src/test/java/com/sam/ApplicationTests.java
+++ src/test/java/com/sam/ApplicationTests.java
@@ -1,13 +1,29 @@
 package com.sam;
 
+import com.sam.dao.UserMapper;
+import com.sam.entity.User;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)//可以在spring环境下运行juint测试
 @SpringBootTest
 class ApplicationTests {
 
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
+        List<User> lists = userMapper.selectList(null);
+        Assert.assertEquals(5, lists.size());
+        lists.forEach(System.out::println);
     }
 
 }
