f03a3789879a0cfdd9a5de19430861ef51994f44
limq <lmq1117@qq.com>
2020-05-17 23:02:30 +0800

    transactional rollback

--- src/test/java/com/sam/ApplicationTests.java
+++ src/test/java/com/sam/ApplicationTests.java
@@ -1,13 +1,14 @@
 package com.sam;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sam.dao.UserMapper;
 import com.sam.entity.User;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +20,7 @@ class ApplicationTests {
     @Autowired
     private UserMapper userMapper;
 
-    //@Test
+//    @Test
     void contextLoads() {
 
         System.out.println("###############");
@@ -29,31 +30,52 @@ class ApplicationTests {
 
     }
 
-    @Test
+//    @Test
+    @Transactional
+    @Rollback(false)
     void truncate(){
         int i = userMapper.truncate();
-        System.out.println("清空(truncate)表成功");
+        System.out.println("清空表成功，清除条数:"+i);
     }
 
 
+    //提交事务 测试结果保存在数据库
     @Test
+    @Transactional
+    @Rollback(true)
     void seed(){
         List<User> data = Arrays.asList(
-                new User(1,"刘备", 58),
-                new User(2,"关羽", 55),
-                new User(3,"张飞", 51),
-                new User(4,"赵云", 38),
-                new User(5,"黄忠", 70),
-                new User(6,"马超", 32),
-                new User(7,"孔明", 50),
-                new User(8,"糜夫人", 57),
-                new User(9,"孙夫人", 32),
-                new User(10,"阿斗", 22),
-                new User(11,"曹操", 58),
-                new User(12,"许褚", 58),
-                new User(13,"荀彧", 58),
-                new User(14,"张辽", 58),
-                new User(15,"曹丕", 30)
+                new User("刘备", 58),
+                new User("关羽", 55),
+                new User("张飞", 51),
+                new User("赵云", 38),
+                new User("黄忠", 70),
+                new User("马超", 32),
+                new User("孔明", 50),
+                new User("糜夫人", 57),
+                new User("孙夫人", 32),
+                new User("阿斗", 22),
+                new User("曹操", 58),
+                new User("许褚", 58),
+                new User("荀彧", 58),
+                new User("张辽", 58),
+                new User("曹丕", 30)
+        );
+
+        for(User user:data){
+            userMapper.insert(user);
+        }
+
+    }
+
+    //不提交事务 测试结果不保存在数据库
+//    @Test
+    @Transactional
+    @Rollback()
+    void seedRollback(){
+        List<User> data = Arrays.asList(
+                new User("刘邦", 58)
+
         );
 
         for(User user:data){
@@ -63,24 +85,14 @@ class ApplicationTests {
     }
 
     @Test
-    void  unSeed(){
-        User u = new User("曹丕",30);
-        QueryWrapper<User> wrapper = new QueryWrapper<>(u);
-        userMapper.delete(wrapper);
-
-        //List<User> users = userMapper.selectList(wrapper);
-        //for(User user:users){
-        //
-        //}
+    void hello(){
+        User user = new User();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Test
-    void  selectMC(){
-        System.out.println("test seed ######");
-        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("age",32);
-        userMapper.selectList(queryWrapper).forEach(System.out::println);
-        //users.forEach(System.out::println);
-    }
 
 }
