3a1e9efed4cb7bf0165dc26c91e71a42d7131e45
limq <limq@cgnfs.com.cn>
2020-05-18 17:19:09 +0800

    hello service db seed test

--- src/test/java/com/sam/DbSeedTests.java
+++ src/test/java/com/sam/DbSeedTests.java
@@ -2,6 +2,7 @@ package com.sam;
 
 import com.sam.dao.UserMapper;
 import com.sam.database.seeds.DbSeed;
+import org.apache.velocity.app.VelocityEngine;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,15 @@ class DbSeedTests {
     @Autowired
     DbSeed dbSeed;
 
+
     @Autowired
     UserMapper userMapper;
+
+    //@Test
+    //void iSeed(){
+    //    new VelocityEngine();
+    //}
+
 
     @Test
     void up(){
