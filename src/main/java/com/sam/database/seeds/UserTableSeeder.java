8995ed5034e11a4c4c9558849642f44d51074866
limq <limq@cgnfs.com.cn>
2020-05-21 15:23:14 +0800

    iseed

--- src/main/java/com/sam/database/seeds/UserTableSeeder.java
+++ src/main/java/com/sam/database/seeds/UserTableSeeder.java
@@ -6,8 +6,13 @@ import java.util.List;
 
 public class UserTableSeeder {
 
-    public static List<User> data = Arrays.asList(
-            new User(1, "刘备", 58, null, null),
-            new User(2, "张飞", 58, null, null)
+public static List<User> data = Arrays.asList(
+        new User(1,"刘备",60,null,null),
+        new User(2,"关羽",58,null,null),
+        new User(3,"张飞",55,null,null),
+        new User(16,"小猫狗2",23,null,null),
+        new User(17,"小花",26,null,null)
     );
+
+
 }
