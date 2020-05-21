eedfdee061d66bbd415e978e4b4ab8920680c1fe
limq <lmq1117@qq.com>
2020-05-22 07:09:25 +0800

    thyme

---
+++ src/main/java/com/sam/controller/ThymeUserController.java
@@ -0,0 +1,28 @@
+package com.sam.controller;
+
+import com.sam.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 测试模板引擎的控制器
+ * @author kevin
+ * @date 2020/5/21
+ */
+@Controller
+public class ThymeUserController {
+
+    @GetMapping("/thyme.html")
+    public String thymeData(Model model){
+        User user = new User();
+        user.setId(1L);
+        user.setName("sam");
+        user.setAge(18);
+        user.setEmail("sam@qq.com");
+
+        model.addAttribute("user",user);
+
+        return "home/thyme.html";
+    }
+}
